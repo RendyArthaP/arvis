267ff9ff9f62423181c8fe3a9dfca6227a78e84a
Rendy Artha <rendyartha312@gmail.com>
2021-05-27 16:29:54 +0700

    get product by category

--- src/redux/actions/product.actions.js
+++ src/redux/actions/product.actions.js
@@ -4,6 +4,10 @@ export const GET_ALL_PRODUCT_REQUEST = 'GET_PRODUCT_REQUEST';
 export const GET_ALL_PRODUCT_SUCCESS = 'GET_PRODUCT_SUCCESS';
 export const GET_ALL_PRODUCT_ERROR = 'GET_PRODUCT_ERROR';
 
+export const GET_PRODUCT_BY_CATEGORY_REQUEST = 'GET_PRODUCT_BY_CATEGORY_REQUEST';
+export const GET_PRODUCT_BY_CATEGORY_SUCCESS = 'GET_PRODUCT_BY_CATEGORY_SUCCESS';
+export const GET_PRODUCT_BY_CATEGORY_ERROR = 'GET_PRODUCT_BY_CATEGORY_ERROR';
+
 export const getAllProductRequest = () => {
   return {
     type: GET_ALL_PRODUCT_REQUEST
@@ -24,6 +28,26 @@ export const getAllProductError = (error) => {
   }
 }
 
+export const getProductByCategoryRequest = () => {
+  return {
+    type: GET_PRODUCT_BY_CATEGORY_REQUEST
+  }
+}
+
+export const getProductByCategorySuccess = (result) => {
+  return {
+    type: GET_PRODUCT_BY_CATEGORY_SUCCESS,
+    result
+  }
+}
+
+export const getProductByCategoryError = (error) => {
+  return {
+    type: GET_PRODUCT_BY_CATEGORY_ERROR,
+    error
+  }
+}
+
 export const getAllProduct = () => {
   return function(dispatch) {
     dispatch(getAllProductRequest())
@@ -32,5 +56,16 @@ export const getAllProduct = () => {
       .get(process.env.REACT_APP_PRODUCT)
       .then((result) => dispatch(getAllProductSuccess(result.data)))
       .catch((error) => dispatch(getAllProductError(error)))
+  }
+}
+
+export const getProductByCategory = (category) => {
+  return function(dispatch) {
+    dispatch(getProductByCategoryRequest())
+
+    axios
+      .get(`${process.env.REACT_APP_PRODUCT_BY_CATEGORY}/${category}`)
+      .then((result) => dispatch(getProductByCategorySuccess(result.data)))
+      .catch((error) => dispatch(getProductByCategoryError(error)))
   }
 }
