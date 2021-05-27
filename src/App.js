3d4f4e553184f358500f094f905e11280fb2b11f
Rendy Artha <rendyartha312@gmail.com>
2021-05-27 16:15:46 +0700

    setup react,tailwind redux

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-300">
+        test
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import handleProducts from './product.reducers';
+
+const rootReducers = combineReducers({
+  handleProducts
+})
+
+export default rootReducers
